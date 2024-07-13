110e2741f416ba3555f69a4d6bbcb1e74527a50d
Austtiin <austinstephens103@gmail.com>
2024-07-13 12:05:41 -0500

    setup

--- src/solution/Main.java
+++ src/solution/Main.java
@@ -1,7 +1,30 @@
+/*
+ * Main.java
+ * This is the main class of the program. It contains the main method that runs the program.
+ *
+ * Austin Stephens
+ * Rasmussen University
+ * 07/13/2024
+ * Professor Kumar
+ *
+ * Postfix Implementation in Java
+Instructions: Write a Java program that accepts a postfix expression and evaluate it using a stack.
+*
+* Test your solution with various expressions.
+Test with the following:
+1) 2 3 5 6 + * -
+2) 8 2 7 3 6 1 * - - - +
+3) 7 4 9 8 3 2 2 + + + * * -
+ */
+
 package solution;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Create a new menu object and run the program
+        menu m = new menu();
+        // Run the program "Menu"
+        m.run();
     }
 }
--- src/solution/menu.java
+++ src/solution/menu.java
@@ -1,4 +1,19 @@
+/*
+ * menu.java
+ * This is the menu class of the program. It contains the run method that runs the program.
+ *
+ * Austin Stephens
+ * Rasmussen University
+ * 07/13/2024
+ * Professor Kumar
+ *
+ *
+ */
+
 package solution;
 
 public class menu {
+    public void run() {
+
+    }
 }

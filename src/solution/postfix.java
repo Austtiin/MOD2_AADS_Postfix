a8e410ce34213cee20a76f3e1df7c7417d338a9c
Austtiin <austinstephens103@gmail.com>
2024-07-14 21:08:03 -0500

    cleanup / testing

--- src/solution/postfix.java
+++ src/solution/postfix.java
@@ -20,8 +20,7 @@ public class postfix {
     public String postfix(String expression) {
         //eval
         evaluate(expression);
-
-        //return the expression
+        //return the expression as it is now evaluated / solved
         return expression;
     }
 
@@ -33,20 +32,24 @@ public class postfix {
         //we need to go through each of our 'tokens' in the expression in order to see what is what
         //we will do this by looking at the length of the expression and looping through
         for (int i = 0; i < expression.length(); i++) {
-            //get the current character
-            char c = expression.charAt(i);
+            //get the current character, by looking at what is at that current location.
+            char e = expression.charAt(i);
 
             //We need to look at the character and determine if it is a digit or an operator
             //if the character is a digit, we need to push it onto the stack
-            if (Character.isDigit(c)) {
+            if (Character.isDigit(e)) {
                 stack.push(c - '0');
             } else {
-                //if the character is an operator, we need to pop the last two operands off the stack
+                //Now, if the char we are looking at is an operator,
+                // we need to 'pop' the last two digits off the stack because
+                // we need to perform an operation on them
                 int x = stack.pop();
                 int y = stack.pop();
 
-                //perform the operation
-                switch (c) {
+                //perform the operation, we will just use a switch statement to
+                // determine what operation to perform.
+                //This is where we will perform the operation aswell
+                switch (e) {
                     case '+':
                         stack.push(y + x);
                         break;
